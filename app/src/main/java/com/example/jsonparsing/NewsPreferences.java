19d1c51fedf7aa53a643c4446a2b0cf3915c2214
ABHISHEK RAJ <abhishekthakur8888@gmail.com>
2020-08-20 14:49:55 +0530

    Update NewsPreferences.java

--- app/src/main/java/com/example/jsonparsing/NewsPreferences.java
+++ app/src/main/java/com/example/jsonparsing/NewsPreferences.java
@@ -4,8 +4,8 @@ import android.content.Context;
 import android.net.Uri;
 
 class NewsPreferences {
-    private static final String NEWS_REQUEST_URL = "http://newsapi.org/v2/top-headlines?country=in&apiKey=30ae643ca7f44acd95d7b9481f11a363";
-    private static final String API = "30ae643ca7f44acd95d7b9481f11a363";
+    private static final String NEWS_REQUEST_URL = "Put your News Request Url here";
+    private static final String API = "Put your News API here";
 
     static Uri.Builder getUri(Context context) {
         Uri baseUri = Uri.parse(NEWS_REQUEST_URL);
